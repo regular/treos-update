5ddf245e6110153c5b0a252801d73cb355c7a9f0
Jan Bölsche <regular.gonzales@googlemail.com>
2019-09-06 17:47:11 +0200

    Port update.sh to js (update.js reads issue from tar, if exists)

--- bin/update.js
+++ bin/update.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/* Usage: update [--config PATH-TO-SSB-CONFIG] [--wait SECS] ISSUEFILE REVROOT
+/* Usage: update [--config PATH-TO-SSB-CONFIG] [--wait SECS] [--tmpdir TMPDIR] ISSUEFILE REVROOT
  *
  * ISSUEFILE is the path to treos-issue.json describing the current system
  * REVROOT is an ssb key. If the latest revision of this message differs from
@@ -9,20 +9,24 @@
  * SECS wait number of seconds for update to have settled (during gossip)
 */
 
+const os = require('os')
 const crypto = require('crypto')
-const through2 = require('through2')
 const fs = require('fs')
 const {join, resolve, relative, dirname} = require('path')
+const through2 = require('through2')
 const multicb = require('multicb')
-const client = require('tre-cli-client')
-const {isMsg} = require('ssb-ref')
 const tar = require('tar-stream')
+const mv = require('mv')
 const equal = require('deep-equal')
+const bytes = require('human-size')
 
+const bl = require('bl')
 const toPull = require('stream-to-pull-stream')
 const pull = require('pull-stream')
 const debounce = require('pull-debounce')
-const bytes = require('human-size')
+
+const client = require('tre-cli-client')
+const {isMsg} = require('ssb-ref')
 
 const makeBootloaderConfigFiles = require('../lib/systemd-boot')
 
@@ -36,208 +40,224 @@ client( (err, ssb, conf, keys) => {
     process.exit(1)
   }
 
-  const issueKv = readIssueFile(conf._[0])
-  const issue = getIssue(issueKv)
-  const revRoot = conf._[1]
-  if (!isMsg(revRoot)) {
-    console.error(`${revRoot} is not a valid ssb message reference`)
-    process.exit(1)
-  }
+  const updateTar = conf.output || 'update.tar'
+  const tmpfile = join(conf.tmpdir || os.tmpdir(), crypto.randomBytes(20).toString('hex'))
 
-  const currentSums = getChecksums(getFiles(issue))
-  console.log('Current System')
-  if (issueKv.key) {
-    console.log(`System message id: ${issue.key}`)
-  }
-  console.log(currentSums)
-  console.log('Bootloader')
-  const currentBootloaderConfig = issue.bootloader
-  console.log(JSON.stringify(currentBootloaderConfig, null, 2))
+  readIssueFile(updateTar, conf._[0], (err, issueKv) => {
+    if (err) {
+      console.error(err.message)
+      process.exit(1)
+    }
+    runUpdate(issueKv)
+  })
 
-  /*
-  */
+  function runUpdate(issueKv) {
+    const issue = getIssue(issueKv)
+    const revRoot = conf._[1]
+    if (!isMsg(revRoot)) {
+      console.error(`${revRoot} is not a valid ssb message reference`)
+      process.exit(1)
+    }
 
-  let updateKv
-  pull(
-    ssb.revisions.heads(revRoot, {
-      live: true,
-      sync: false,
-      meta: true,
-      values: true,
-      maxHeads: 1,
-      allowAllAuthors: false
-    }),
-    pull.map( mh =>{
-      const {meta, heads} = mh
-      return heads[0]
-    }),
-    pull.filter(),
-    pull.through(kv => {
-      console.log(`Found update ${kv.key}`)
-      updateKv = kv
-    }),
-    debounce(conf.wait !== undefined ? conf.wait : 3000),
-    pull.through(kv => {
-      console.log('Checking update')
-    }),
-    pull.map(kv => getIssue(kv)),
-    pull.map(issue => {
-      return {
-        files: getFiles(issue),
-        bootloaderConfig: issue.bootloader
-      }
-    }),
-    pull.map( ({files, bootloaderConfig}) => {
-      return {
-        checksums: getChecksums(files),
-        bootloaderConfig
-      }
-    }),
-    pull.asyncMap(({checksums, bootloaderConfig}, cb) =>{
-      pull(
-        pull.values(Object.entries(checksums)),
-        pull.filter( ([filename, checksum]) =>{
-          if (currentSums[filename] == checksum) {
-            console.log(`${filename} is up to date`)
-            return false
-          }
-          console.log(`${filename} needs update`)
-          return true
-        }),
-        pull.map( ([filename, checksum])=>{
-          const currentFile = Object.entries(currentSums).find( ([key, value]) => value == checksum)
-          if (currentFile) return {
-            filename, checksum, source: currentFile[0]
-          }
-          return {filename, checksum}
-        }),
-        pull.collect((err, result)=>{
-          if (err) return cb(err)
-          if (!equal(bootloaderConfig, currentBootloaderConfig)) {
-            console.log('Bootloader config has changed')
-            const bootloaderConfigFiles = makeBootloaderConfigFiles(bootloaderConfig)
-            result = result.concat(Object.entries(bootloaderConfigFiles).map( ([filename, content]) => {
-              return {filename, content}
-            }))
-          } else {
-            console.log('Bootloader config is identical')
-          }
-          cb(null, result)
-        })
-      )
-    }),
-    pull.filter(todo=>{
-      if (!todo.length) {
-        console.log('Nothing to do')
-      } else {
-        console.log('Applying update')
-      }
-      return todo.length
-    }),
-    pull.asyncMap( (todo, cb)=>{
-      const output = conf.output || 'update.tar'
-      console.log(`Writing ${output}`)
-      const pack = tar.pack()
-      const outputStream = fs.createWriteStream(output)
-      pack.pipe(outputStream)
-      pack.entry({name: 'treos-issue.json'}, JSON.stringify(updateKv, null, 2))
-      pull(
-        pull.values(todo),
-        pull.asyncMap( ({filename, checksum, content, source}, cb)=> {
-          if (content) {
-            console.log(`Packing ${filename}`)
-            pack.entry({name: filename}, content)
-            return cb(null)
-          }
-          let [shasum, size] = checksum.split('#')
-          size = Number(size)
-          shasum = shasum.split('.')[0]
-          console.log(`Packing ${filename} (${size} = ${bytes(size)})`)
-          const entry = pack.entry({name: filename, size}, err =>{
-            console.log(`tar entry for ${filename} complete`)
-            if (err) console.error(err.message)
-            cb(err || null)
-          })
-          if (source) {
-            const sourcePath = resolve(conf['boot-dir'] || '/boot', source)
-            console.log(`Reading from ${sourcePath}`)
-            const hash = crypto.createHash('sha256')
-            fs.createReadStream(sourcePath)
-              .pipe(through2(function (chunk, enc, callback) {
-                this.push(chunk)
-                hash.update(chunk)
-                callback()
+    const currentSums = getChecksums(getFiles(issue))
+    console.log('Current System')
+    if (issueKv.key) {
+      console.log(`System message id: ${issueKv.key}`)
+    }
+    console.log(currentSums)
+    console.log('Bootloader')
+    const currentBootloaderConfig = issue.bootloader
+    console.log(JSON.stringify(currentBootloaderConfig, null, 2))
+
+    /*
+    */
+
+    let updateKv
+    pull(
+      ssb.revisions.heads(revRoot, {
+        live: true,
+        sync: false,
+        meta: true,
+        values: true,
+        maxHeads: 1,
+        allowAllAuthors: false
+      }),
+      pull.map( mh =>{
+        const {meta, heads} = mh
+        return heads[0]
+      }),
+      pull.filter(),
+      pull.through(kv => {
+        console.log(`Found update ${kv.key}`)
+        updateKv = kv
+      }),
+      debounce(conf.wait !== undefined ? conf.wait : 3000),
+      pull.through(kv => {
+        console.log('Checking update')
+      }),
+      pull.map(kv => getIssue(kv)),
+      pull.map(issue => {
+        return {
+          files: getFiles(issue),
+          bootloaderConfig: issue.bootloader
+        }
+      }),
+      pull.map( ({files, bootloaderConfig}) => {
+        return {
+          checksums: getChecksums(files),
+          bootloaderConfig
+        }
+      }),
+      pull.asyncMap(({checksums, bootloaderConfig}, cb) =>{
+        pull(
+          pull.values(Object.entries(checksums)),
+          pull.filter( ([filename, checksum]) =>{
+            if (currentSums[filename] == checksum) {
+              console.log(`${filename} is up to date`)
+              return false
+            }
+            console.log(`${filename} needs update`)
+            return true
+          }),
+          pull.map( ([filename, checksum])=>{
+            const currentFile = Object.entries(currentSums).find( ([key, value]) => value == checksum)
+            if (currentFile) return {
+              filename, checksum, source: currentFile[0]
+            }
+            return {filename, checksum}
+          }),
+          pull.collect((err, result)=>{
+            if (err) return cb(err)
+            if (!equal(bootloaderConfig, currentBootloaderConfig)) {
+              console.log('Bootloader config has changed')
+              const bootloaderConfigFiles = makeBootloaderConfigFiles(bootloaderConfig)
+              result = result.concat(Object.entries(bootloaderConfigFiles).map( ([filename, content]) => {
+                return {filename, content}
               }))
-              .pipe(entry)
-              .on('finish', ()=>{
-                const sha = hash.digest('base64')
-                if (sha !== shasum) {
-                  console.error(`checksum mismatch! ${sha} !== ${shasum}`)
-                  process.exit(1)
-                } else {
-                  console.log(`${sourcePath} checksum matches`)
-                }
-              })
-          } else {
-            const blob = `&${shasum}.sha256`
-            console.log(`Loading from blob ${blob}`)
-            
-            ensureLocal(blob, err=>{
-              if (err) {
-                console.error(err.message)
-                return cb(err)
-              }
-              console.log('Received blob')
-              let total = 0 
+            } else {
+              console.log('Bootloader config is identical')
+            }
+            cb(null, result)
+          })
+        )
+      }),
+      pull.filter(todo=>{
+        if (!todo.length) {
+          console.log('Nothing to do')
+        } else {
+          console.log('Applying update')
+        }
+        return todo.length
+      }),
+      pull.asyncMap( (todo, cb)=>{
+        const output = tmpfile
+        console.log(`Writing ${output}`)
+        const pack = tar.pack()
+        const outputStream = fs.createWriteStream(output)
+        pack.pipe(outputStream)
+        pack.entry({name: 'treos-issue.json'}, JSON.stringify(updateKv, null, 2))
+        pull(
+          pull.values(todo),
+          pull.asyncMap( ({filename, checksum, content, source}, cb)=> {
+            if (content) {
+              console.log(`Packing ${filename}`)
+              pack.entry({name: filename}, content)
+              return cb(null)
+            }
+            let [shasum, size] = checksum.split('#')
+            size = Number(size)
+            shasum = shasum.split('.')[0]
+            console.log(`Packing ${filename} (${size} = ${bytes(size)})`)
+            const entry = pack.entry({name: filename, size}, err =>{
+              console.log(`tar entry for ${filename} complete`)
+              if (err) console.error(err.message)
+              cb(err || null)
+            })
+            if (source) {
+              const sourcePath = resolve(conf['boot-dir'] || '/boot', source)
+              console.log(`Reading from ${sourcePath}`)
               const hash = crypto.createHash('sha256')
-              pull(
-                ssb.blobs.get(blob),
-                pull.through(x=>{
-                  total += x.length
-                  process.stdout.write(`${total} / ${size}\r`)
-                }),
-                pull.through(x=>{
-                  hash.update(x)
-                }),
-                toPull.sink(entry, err =>{
+              fs.createReadStream(sourcePath)
+                .pipe(through2(function (chunk, enc, callback) {
+                  this.push(chunk)
+                  hash.update(chunk)
+                  callback()
+                }))
+                .pipe(entry)
+                .on('finish', ()=>{
                   const sha = hash.digest('base64')
                   if (sha !== shasum) {
                     console.error(`checksum mismatch! ${sha} !== ${shasum}`)
                     process.exit(1)
                   } else {
-                    console.log(`${filename} checksum matches`)
+                    console.log(`${sourcePath} checksum matches`)
                   }
-                  if (err) console.error(err.message)
                 })
-              )
-            })
-          }
-        }),
-        pull.onEnd(err=>{
-          if (err) return cb(err)
-          pack.finalize()
-          outputStream.on('close', err => {
+            } else {
+              const blob = `&${shasum}.sha256`
+              console.log(`Loading from blob ${blob}`)
+              
+              ensureLocal(blob, err=>{
+                if (err) {
+                  console.error(err.message)
+                  return cb(err)
+                }
+                let total = 0 
+                const hash = crypto.createHash('sha256')
+                pull(
+                  ssb.blobs.get(blob),
+                  pull.through(x=>{
+                    total += x.length
+                    process.stdout.write(`${total} / ${size}\r`)
+                  }),
+                  pull.through(x=>{
+                    hash.update(x)
+                  }),
+                  toPull.sink(entry, err =>{
+                    const sha = hash.digest('base64')
+                    if (sha !== shasum) {
+                      console.error(`checksum mismatch! ${sha} !== ${shasum}`)
+                      process.exit(1)
+                    } else {
+                      console.log(`${filename} checksum matches`)
+                    }
+                    if (err) console.error(err.message)
+                  })
+                )
+              })
+            }
+          }),
+          pull.onEnd(err=>{
             if (err) return cb(err)
-            cb(null, output)
+            pack.finalize()
+            outputStream.on('close', err => {
+              if (err) return cb(err)
+              cb(null, output)
+            })
           })
+        )
+      }),
+      pull.drain(outputFile=>{
+        console.log(`Written ${outputFile}, moving to ${updateTar}`)
+        mv(outputFile, updateTar, err =>{
+          if (err) {
+            console.error(err.message)
+            ssb.close()
+            process.exit(1)
+          }
+          ssb.close()
+          console.log('Done')
+          process.exit(0)
         })
-      )
-    }),
-    pull.drain(outputFile=>{
-      console.log(`Written ${outputFile}.`)
-      if (!conf.live) {
+      }, err=>{
+        if (err && err !== true) {
+          console.error(err.message)
+          process.exit(1)
+        }
         ssb.close()
-        process.exit(0)
-      }
-    }, err=>{
-      if (err && err !== true) {
-        console.error(err.message)
-        process.exit(1)
-      }
-      ssb.close()
-      console.log('Done')
-    })
-  )
+      })
+    )
+  }
 
   function ensureLocal(blob, cb) {
     ssb.blobs.has(blob, (err, has) => {
@@ -247,24 +267,61 @@ client( (err, ssb, conf, keys) => {
         return cb(null)
       }
       console.log('Requesting blob ...')
-      ssb.blobs.want(blob, cb) 
+      ssb.blobs.want(blob, (err, has)=>{
+        if (err) return cb(err)
+        console.log('Received blob')
+        cb(null, has)
+      }) 
     })
   }
 
 })
   
-function readIssueFile(path) {
-  const issueFile = resolve(path)
-  console.error('issue:', issueFile)
+function extractIssue(updateTar, cb) {
+  const extract = tar.extract()
+  const buffer = bl()
+  let found =false
+  extract.on('entry', function(header, stream, next) {
+    const {name} = header
+    stream.on('end', function() {
+      next() // ready for next entry
+    })
+    if (name == 'treos-issue.json') {
+      console.log('Found treos-issue.json')
+      stream.pipe(buffer)
+      found = true
+    } else {
+      stream.resume() // just auto drain the stream
+    }
+  })                 
+  extract.on('finish', function() {
+    console.log('Finished reading tar')
+    cb(found ? null : new Error('No entry for treos-issue.json found in tar'), buffer.toString('utf8'))
+  })
+  fs.createReadStream(updateTar).pipe(extract)
+}
 
-  let issue
-  try {
-    issue = JSON.parse(fs.readFileSync(issueFile))
-  } catch(err) {
-    console.error(err.message)
-    process.exit(1)
+function readIssueFile(updateTar, path, cb) {
+  if (fs.existsSync(updateTar)) {
+    console.log('Output file exists. Reading packed treos-issue.json')
+    extractIssue(updateTar, handleContent)
+  } else {
+    const issueFile = resolve(path)
+    console.error(`reading ${issueFile}`)
+    const fileContent = fs.readFileSync(issueFile)
+    handleContent(null, fileContent)
   }
-  return issue
+
+  function handleContent(err, fileContent) {
+    if (err) return cb(err)
+    let issue
+    try {
+      issue = JSON.parse(fileContent)
+    } catch(err) {
+      return cb(err)
+    }
+    cb(null, issue)
+  }
 }
 
 function getIssue(kv) {
